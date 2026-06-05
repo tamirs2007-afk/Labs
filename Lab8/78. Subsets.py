52cc3e0034b16b52b371a092e80f35f59aa13c83
Tamir <tamir.s.2007@gmail.com>
2026-06-05 22:11:36 +1000

    78

---
+++ Lab8/78. Subsets.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = [[]]
+        
+        for num in nums:
+            res += [curr + [num] for curr in res]
+            
+        return res
